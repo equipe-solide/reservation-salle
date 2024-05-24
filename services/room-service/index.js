4ff1235ab737d8f431f2ba0f2149b1e5ca49fc57
Raphaël NK <raphael.nk.dev@gmail.com>
2024-05-24 18:55:09 +0300

    added proto file in parent directory

---
+++ services/room-service/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, '../../protos/room.proto'));
+// const degreeProto = grpc.loadPackageDefinition(packageDefinition);
+
+// const DEGREE = [
+//     {
+//         id: 100,
+//         degreeId: 1000,
+//         title: 'Engineering',
+//         major: 'Electronics'
+//     }
+// ];
+
+function findDegree(call, callback) {
+    // let degree = DEGREE.find((degree) => degree.degreeId == call.request.id);
+    // if(degree) {
+    //     callback(null, degree);
+    // }
+    // else {
+    //     callback({
+    //         message: 'Degree not found',
+    //         code: grpc.status.INVALID_ARGUMENT
+    //     });
+    // }
+}
+
+const server = new grpc.Server();
+// server.addService(degreeProto.Degrees.service, { find: findDegree });
+
+server.bindAsync('0.0.0.0:50052', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+});
